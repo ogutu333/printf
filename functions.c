aedfbc17925a514e4ae5c94e6225867b7e00d8c3
pkonsomu2020 <pkonsomu2020@gmail.com>
2022-11-22 03:11:45 -0800

    Task 1

--- functions.c
+++ functions.c
@@ -108,7 +108,7 @@ int print_percent(va_list types, char buffer[],
  * @size: Size specifier
  * Return: Number of chars printed
  */
-int print_int(va_list, types, char buffer[],
+int print_int(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
 	int i = BUFF_SIZE - 2;
